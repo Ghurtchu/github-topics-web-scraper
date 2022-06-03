b87aec47243d27b4dfc8c2a8b784e39f86626bab
nghurtch <nghurtch@broadinstitute.com>
2022-06-03 13:29:15 +0400

    refactor code, shrink methods, remove unused methods

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,28 +4,40 @@ import zio.console.*
 import java.io.IOException
 
 object Main extends zio.App {
+
+  given baseUrl: URL = URL("https://github.com")
+
+  private val optionMapping: Map[String, String] = Map(
+    "1" -> "Download, parse and store top 30 most popular projects by topics as a CSV file.",
+    "2" -> "Exit."
+  )
+
   override def run(args: List[String]): URIO[ZEnv, ExitCode] = program.exitCode
 
 
   val program: ZIO[Console, Throwable, Unit] = for {
-    _                <- putStrLn("Welcome to ScalaJobs.com Scraper")
-    _                <- putStrLn("Choose one of the following options")
-    _                <- displayOptions()
-    userOption       <- chooseOption()
-    _                <- Scraper.scrape()
+    _          <- putStrLn("Welcome to Github Scraper") *> putStrLn("Choose one of the following options")
+    _          <- displayOptions()
+    userOption <- chooseOption()
+    _          <- processAndThenExecute(userOption)
   } yield ()
 
-  private def displayOptions(): ZIO[Console, IOException, Unit] = {
-    putStrLn("Options: ") *>
-    putStrLn("1 - Download all data") *>
-    putStrLn("2 - Exit")
+  private def displayOptions(): ZIO[Console, IOException, Unit] = ZIO.succeed {
+    optionMapping.foreach((k, v) => println(k concat " = " concat v))
   }
 
   private def chooseOption(): ZIO[Console, IOException, String] = for {
     choice <- getStrLn
-    _      <- putStrLn(s"Executing command: $choice")
+    _      <- putStrLn(s"Executing command: ${optionMapping.getOrElse(choice, "Exit")}")
   } yield choice
 
+  private def processAndThenExecute(userOption: String)(using url: URL): ZIO[Console, Throwable, Unit] =
+    userOption match
+      case "1" => Scraper.scrape(url.value)
+      case _   => ZIO.succeed(())
+
+
+  case class URL(value: String)
 
 }
 
--- src/main/scala/Scraper.scala
+++ src/main/scala/Scraper.scala
@@ -5,43 +5,20 @@ import org.jsoup.select.{Elements, NodeFilter}
 import zio.*
 import zio.console.*
 
+import java.io.File
 import scala.jdk.CollectionConverters.*
 import java.util.stream.Collectors
 import scala.concurrent.Future
 
 object Scraper {
 
-  val baseUrl = "https://github.com"
-  val topicsUrl: String = baseUrl concat "/topics/"
   val paragraphTag = "p"
   val href = "href"
   val descriptionClassName = "f5 color-fg-muted mb-0 mt-1"
   val paragraphTitleClassName = "f3 lh-condensed mb-0 mt-1 Link--primary"
 
-  def filterElements(paragraphs: List[Element])(f: Element => Boolean): Task[List[Element]] = Task.effect(paragraphs.filter(f))
-
-  def mapParagraphs[A](paragraphs: List[Element])(f: Element => A): Task[List[A]] = Task.effect(paragraphs.map(f))
-
-  def fetchDocByURL(url: String): Task[Document] = Task.effect(Jsoup.connect(url).get())
-
-  def extractBody(doc: Document): Task[Element] = Task.effect(doc.body())
-
-  def selectAllParagraphs(body: Element): Task[List[Element]] = Task.effect(body.select(paragraphTag).stream().collect(Collectors.toList).asScala.toList)
-
-  def extractUrls(hyperlinks: List[Element])(url: String): Task[List[String]] = Task.effect {
-    val href = "href"
-
-    hyperlinks.map(elem => url concat elem.attr(href))
-  }
-
-  def combine(titles: List[String], descriptions: List[String], urls: List[String]): UIO[List[Seq[String]]] = ZIO.succeed {
-    titles.zip(descriptions.zip(urls)).map { tuple =>
-      Seq(tuple._1, tuple._2._1, tuple._2._2)
-    }
-  }
-
-  def scrape(): ZIO[Console, Throwable, Unit] = for {
-    doc             <- fetchDocByURL(topicsUrl)
+  final def scrape(baseUrl: String): ZIO[Console, Throwable, Unit] = for {
+    doc             <- fetchDocByURL(baseUrl concat "/topics/")
     body            <- extractBody(doc)
     allParagraphs   <- selectAllParagraphs(body)
     titleParagraphs <- filterElements(allParagraphs)(_.hasClass(paragraphTitleClassName))
@@ -49,19 +26,43 @@ object Scraper {
     hyperlinks      <- parseToHyperLinks(body)
     descriptions    <- filterElements(allParagraphs)(_.hasClass(descriptionClassName)).flatMap(mapParagraphs(_)(_.text()))
     urls            <- extractUrls(hyperlinks)(baseUrl)
-    topicDocuments  <- ZIO.foreachPar(urls)(url => ZIO.effect(Jsoup.connect(url).get()))
+    topicDocuments  <- fetchTopicPagesInParallel(urls)
     namePairList    <- parseToUserNameAndProjectTitle(topicDocuments)
     partialData     <- combine(titles, descriptions, urls)
     fullData        =  partialData.zip(namePairList).map(_ concat _)
-    _               <- putStrLn(s"full data = $fullData")
-    _               <- putStrLn(s"full data length = ${partialData.length}")
-    csv             <- ZIO.effect {
-                      val file = new java.io.File("data.csv")
-                      val writer = CSVWriter.open(file)
-                      writer.writeRow(Seq("title", "description", "url", "user_name", "project_title", "stars"))
-                      writer.writeAll(fullData)
-                    }
+    _               <- generateCSV(fullData)
   } yield ()
+
+  private def generateCSV(fullData: List[Seq[String]]): Task[Unit] = Task.effect {
+    val file = new File("data.csv")
+    val writer = CSVWriter.open(file)
+    writer.writeRow(Seq("title", "description", "url", "user_name", "project_title"))
+    writer.writeAll(fullData)
+  }
+
+  private def fetchTopicPagesInParallel(urls: List[String]): ZIO[Any, Throwable, List[Document]] = ZIO.foreachPar(urls)(url => ZIO.effect(Jsoup.connect(url).get()))
+
+  private def filterElements(paragraphs: List[Element])(f: Element => Boolean): Task[List[Element]] = Task.effect(paragraphs.filter(f))
+
+  private def mapParagraphs[A](paragraphs: List[Element])(f: Element => A): Task[List[A]] = Task.effect(paragraphs.map(f))
+
+  private def fetchDocByURL(url: String): Task[Document] = Task.effect(Jsoup.connect(url).get())
+
+  private def extractBody(doc: Document): Task[Element] = Task.effect(doc.body())
+
+  private def selectAllParagraphs(body: Element): Task[List[Element]] = Task.effect(body.select(paragraphTag).stream().collect(Collectors.toList).asScala.toList)
+
+  private def extractUrls(hyperlinks: List[Element])(url: String): Task[List[String]] = Task.effect {
+    val href = "href"
+
+    hyperlinks.map(elem => url concat elem.attr(href))
+  }
+
+  private def combine(titles: List[String], descriptions: List[String], urls: List[String]): UIO[List[Seq[String]]] = ZIO.succeed {
+    titles.zip(descriptions.zip(urls)).map { tuple =>
+      Seq(tuple._1, tuple._2._1, tuple._2._2)
+    }
+  }
 
 
   private def parseToUserNameAndProjectTitle(topicDocuments: List[Document]): Task[List[List[String]]] = Task.effect {
